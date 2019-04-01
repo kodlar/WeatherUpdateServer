1377eaec939d65d2ed9998f5d0eabec5c3a95c93
okeskiner <ogankeskiner@gmail.com>
2019-04-01 17:42:00 +0300

    Add project files.

---
+++ WeatherUpdateServer/Model/WeatherItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherUpdateServer.Model
+{
+    public class WeatherItem
+    {
+        public int Id { get; set; }
+        public  WeatherStatus Status { get; set; }
+        public int Temperature { get; set; }
+        public string City { get; set; }
+
+    }
+
+    public enum WeatherStatus
+    {
+        Sun,
+        Fog,
+        Rain
+    }
+}
